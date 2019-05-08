7b18d67dd7b9e26967a74eda5d334d9b13f37449
AnthonyOkros <12750954@student.uts.edu.au>
2019-05-08 18:46:03 +1000

    Edit to result in one line output

--- RaspiUpsMonitor/RaspiUpsMonitor/max17034.py
+++ RaspiUpsMonitor/RaspiUpsMonitor/max17034.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 from smbus2 import SMBusWrapper
-import struct 
+import struct
+import sys
+import time
 
 from config import Config
 
@@ -26,13 +28,46 @@ class Max17034:
 		swapped  = struct.unpack("<H", struct.pack(">H", read))[0]
 		capacity = swapped/256
 		return capacity
-
-if __name__ == '__main__':
+	
+while True:
+	try:
+	
+		if __name__ == '__main__':
+			# 0 = /dev/i2c-0 (port I2C0)
+			# 1 = /dev/i2c-1 (port I2C1)
+			bus = 1
+			# MAX17043
+			address = 0x36
+			max17034 = Max17034(address,bus)
+			
+			VOLT = '%(volt).2fV' % {'volt': max17034.readVoltage()}
+			CAP = '(%(cap)i%%)' % {'cap': max17034.readCapacity()}
+			for i in [VOLT]:
+				sys.stdout.write("\r" + i)
+				sys.stdout.flush()
+				time.sleep(1)
+			for i in [CAP]:
+				sys.stdout.write("\r" + i)
+				sys.stdout.flush()
+				time.sleep(1)
+	except KeyboardInterrupt:
+		exit()
+                    
+        #print("Current Voltage: " + VOLT + " Current capacity: " + CAP)
+        
+#if __name__ == '__main__':
 	# 0 = /dev/i2c-0 (port I2C0)
 	# 1 = /dev/i2c-1 (port I2C1)
-	bus = 1
+#	bus = 1
 	# MAX17043
-	address = 0x36
-
-	max17034 = Max17034(address,bus)
-	print ( '%(volt).2fV (%(cap)i%%)' % { 'volt': max17034.readVoltage(), 'cap': max17034.readCapacity() } )
+#	address = 0x36
+	# Defining loop to show battery levels
+#	while True:
+#		try:
+#			max17034 = Max17034(address,bus)
+#			for i in ['%(volt).2fV (%(cap)i%%)' % { 'volt': max17034.readVoltage(), 'cap': max17034.readCapacity() } )]:
+#				sys.stdout.write("\r" + i)
+#				sys.stdout.flush()
+#				time.sleep(1)
+#		except KeyboardInterrupt:
+#			print("Quitting script")
